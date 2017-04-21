20aba0aebedeea2fdc6566aad4f2b917fef050b0
FrankenApps <de.frankenapps@gmail.com>
2017-04-21 12:29:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Epicycloide
+
+This is a little web project with Javascript and D3.js. It is a visualization of the creation of an epicycloide. You can also discover the nice patterns that can be produced.
+
+## TODO
+
+1. Add an offset so that epitrochoids can be produced too.
+2. Make the controls more mobile friendly.
---
+++ script.js
@@ -0,0 +1,156 @@
+var speed_adjustment = 0.1
+var speed = 1*speed_adjustment;
+var play = speed;
+
+var animation;
+
+//svg globals
+var layer0, layer1;
+var mover1, mover2, mover3, mover4;
+
+//viewport
+var height = 0;
+var width = $(window).width();
+
+// circle settings
+var innerRadius = 100;
+var outerRadius = 50;
+
+// rolling condition: omega = v/r
+var circ_movement = 0;
+var rotation = 0;
+var offset = 0;
+
+$( document ).ready(function() {
+
+  jQuery.fn.extend({
+    disable: function(state) {
+        return this.each(function() {
+            this.disabled = state;
+        });
+    }
+  });
+
+  var switchery = new Switchery(document.getElementById('trace'), { size: 'small' });
+
+  var switchery2 = new Switchery(document.getElementById('trace_only'), { size: 'small' });
+
+  $("#time").bootstrapSlider();
+  $("#time").on("slide", function(slideEvt) {
+	speed = slideEvt.value*speed_adjustment;
+});
+
+$("#radius").bootstrapSlider();
+$("#radius").on("slide", function(slideEvt) {
+outerRadius = slideEvt.value;
+mover4.attr("y1", height/2-innerRadius-outerRadius);
+mover3.attr('cy', height/2-innerRadius-outerRadius*2).attr('r', outerRadius/10);
+mover2.attr('cy', height/2-innerRadius-outerRadius).attr('r', outerRadius/10);
+mover1.attr('cy', height/2-innerRadius-outerRadius).attr('r', outerRadius);
+});
+
+$('#stop').click(function() {
+  play=speed;
+  speed=0;
+  $('#stop').disable(true);
+  $("#time").bootstrapSlider("disable");
+});
+
+$('#play').click(function() {
+  $('#stop').disable(false);
+  $("#time").bootstrapSlider("enable");
+  speed=play;
+});
+
+  svg = d3.select("body")
+            .append("svg")
+            .attr("width", '100%')
+            .attr("height", '100%')
+            .call(d3.zoom().on("zoom", function () {
+              svg.attr("transform", d3.event.transform);
+            }))
+        .append("g");
+
+            height = $(window).height();
+            var layer_1 = svg.append('g')
+            layer0 = layer_1.append('g')
+            layer1 = layer0.append('g');
+
+            var centerCircle = layer1.append('circle')
+            .attr('cx', width/2)
+            .attr('cy', height/2)
+            .attr('r', innerRadius)
+            .attr('class', 'visual')
+            .style('fill', '#FF7F50');
+
+            var middleCenterCircle = layer1.append('circle')
+            .attr('cx', width/2)
+            .attr('cy', height/2)
+            .attr('r', innerRadius/15)
+            .attr('class', 'visual')
+            .style('fill', '#DF5F30');
+
+            mover1 = layer1.append('circle')
+            .attr('cx', width/2)
+            .attr('cy', height/2-innerRadius-outerRadius)
+            .attr('r', outerRadius)
+            .attr('class', 'visual')
+            .style('fill', '#4169E1');
+
+            mover2 = layer_1.append('circle')
+            .attr('cx', width/2)
+            .attr('cy', height/2-innerRadius-outerRadius)
+            .attr('r', outerRadius/10)
+            .attr('class', 'visual')
+            .style('fill', '#000080');
+
+            mover3 = layer_1.append('circle')
+            .attr('cx', width/2)
+            .attr('cy', height/2-innerRadius-2*outerRadius)
+            .attr('r', outerRadius/10)
+            .style('fill', '#FF0000');
+
+            mover4 = layer0.append("line")
+            .attr("x1", width/2)
+            .attr("y1", height/2-innerRadius-outerRadius)
+            .attr("x2", width/2)
+            .attr("y2", height/2-innerRadius-2*outerRadius)
+            .attr("stroke-width", 2)
+            .attr('class', 'visual')
+            .attr("stroke", "#FF7F50");
+
+
+
+    animation = setInterval(controller, 10);  //start controller
+});
+
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
+}
+
+function controller(){
+  rotation+=speed*(innerRadius/outerRadius+1);   //T=360*speed
+  circ_movement+=speed;
+
+  mover1.attr('cx', Math.sin(toRadians(circ_movement))*(innerRadius+outerRadius)+width/2).attr('cy', -Math.cos(toRadians(circ_movement))*(innerRadius+outerRadius)+height/2);
+  mover2.attr('cx', Math.sin(toRadians(circ_movement))*(innerRadius+outerRadius)+width/2).attr('cy', -Math.cos(toRadians(circ_movement))*(innerRadius+outerRadius)+height/2);
+  mover3.attr('cx', Math.sin(toRadians(circ_movement))*(innerRadius+outerRadius)+Math.sin(toRadians(rotation))*outerRadius+width/2).attr('cy', -Math.cos(toRadians(circ_movement))*(innerRadius+outerRadius)-Math.cos(toRadians(rotation))*outerRadius+height/2);
+  mover4.attr('x1', Math.sin(toRadians(circ_movement))*(innerRadius+outerRadius)+width/2).attr('y1', -Math.cos(toRadians(circ_movement))*(innerRadius+outerRadius)+height/2).attr('x2', Math.sin(toRadians(circ_movement))*(innerRadius+outerRadius)+Math.sin(toRadians(rotation))*outerRadius+width/2).attr('y2', -Math.cos(toRadians(circ_movement))*(innerRadius+outerRadius)-Math.cos(toRadians(rotation))*outerRadius+height/2);
+
+  if($('#trace').is(":checked")){
+    layer0.append('circle').attr('cx', Math.sin(toRadians(circ_movement))*(innerRadius+outerRadius)+Math.sin(toRadians(rotation))*outerRadius+width/2).attr('cy', -Math.cos(toRadians(circ_movement))*(innerRadius+outerRadius)-Math.cos(toRadians(rotation))*outerRadius+height/2).attr('r', outerRadius/25).style('fill', '#FF0000').attr('class', 'trace');
+  }
+  if($('#trace_only').is(":checked")){
+    $('.visual').hide('400', function() {
+
+    });
+  } else {
+    $('.visual').show('400', function() {
+
+    });
+  }
+}
+
+function removeTrace() {
+    $('.trace').remove();
+}
